5107751deb584c2f86c1cac957956cf17c6e46ec
Dominik Bujnowicz <dominik.bujnowicz2@gmail.com>
2022-11-05 18:25:38 +0100

    making list draggable

--- lab3/script.js
+++ lab3/script.js
@@ -4,3 +4,38 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+let draggedElement = null
+
+const listElements = Array.from(document.querySelectorAll('.list-element'));
+
+function getElemenetAfterDraggedElement(mouseY) {
+    const availableElements = listElements.filter(listElement => listElement !== draggedElement)
+    return availableElements.reduce((closestElement, availableElement) => {
+        const availableElementRectangle = availableElement.getBoundingClientRect();
+        const offset = mouseY - availableElementRectangle.top - availableElementRectangle.height / 2;
+        return (offset < 0 && offset > closestElement.offset) ? { offset: offset, element: availableElement } : closestElement;
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+listElements.forEach(listElement => {
+
+    listElement.addEventListener('dragstart', (event) => {
+        listElement.style.opacity = '0.5';
+        // listElement.style.borderStyle = 'dashed';
+        draggedElement = listElement;
+    });
+
+    listElement.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const elementAfterDraggedElement = getElemenetAfterDraggedElement(event.clientY);
+        if (elementAfterDraggedElement !== null) document.getElementById('list').insertBefore(draggedElement, elementAfterDraggedElement);
+    });
+
+    listElement.addEventListener('dragend', (event) => {
+        listElement.style.opacity = '1';
+        // listElement.style.borderStyle = 'solid';
+    });
+
+});
