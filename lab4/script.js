c9f44b5251a90944691a73d15f0f13f028df68f0
DominikBuj <dominik.bujnowicz2@gmail.com>
2022-11-19 19:03:35 +0100

    working load button

--- lab4/script.js
+++ lab4/script.js
@@ -10,37 +10,20 @@ open.onupgradeneeded = function() {
     let store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
 };
 
-open.onsuccess = function() {
-    // Start a new transaction
+function loadData() {
+
     var db = open.result;
     var tx = db.transaction("MyObjectStore", "readwrite");
     var store = tx.objectStore("MyObjectStore");
-    var index = store.index("NameIndex");
 
-    // Add some data
-    store.put({id: 12345, name: {first: "John", last: "Doe"}, age: 42});
-    store.put({id: 67890, name: {first: "Bob", last: "Smith"}, age: 35});
-    
-    // Query the data
-    var getJohn = store.get(12345);
-    var getBob = index.get(["Smith", "Bob"]);
+    let getData = store.get(1);
 
-    getJohn.onsuccess = function() {
-        console.log(getJohn.result.name.first);  // => "John"
+    getData.onsuccess = function() {
+        for (let fieldName of fieldNames) {
+            document.getElementById(fieldName).value = getData.result.data[fieldName];
+        }
     };
 
-    getBob.onsuccess = function() {
-        console.log(getBob.result.name.first);   // => "Bob"
-    };
-
-    // // Close the db when the transaction is done
-    // tx.oncomplete = function() {
-    //     db.close();
-    // };
-}
-
-function loadData() {
-    
 }
 
 function saveData() {
